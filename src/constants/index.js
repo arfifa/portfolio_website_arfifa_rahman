55f8c9f276a6cea7e0fe3a359a4bc4e56dc5a21e
Arfifa Rahman <arfifarahman509@gmail.com>
2023-10-06 16:35:40 +0700

    first develop my portfolio

---
+++ src/constants/index.js
@@ -0,0 +1,210 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+    mediatron,
+    digistar,
+} from "../assets";
+
+export const navLinks = [
+    {
+        id: "about",
+        title: "Tentang Saya",
+    },
+    {
+        id: "work",
+        title: "Pekerjaan",
+    },
+    {
+        id: "contact",
+        title: "Kontak",
+    },
+];
+
+const services = [
+    {
+        title: "Web Developer",
+        icon: web,
+    },
+    {
+        title: "React Native Developer",
+        icon: mobile,
+    },
+    {
+        title: "Fullstack Javascript Developer",
+        icon: backend,
+    },
+];
+
+const technologies = [
+    {
+        name: "HTML 5",
+        icon: html,
+    },
+    {
+        name: "CSS 3",
+        icon: css,
+    },
+    {
+        name: "JavaScript",
+        icon: javascript,
+    },
+    {
+        name: "React JS",
+        icon: reactjs,
+    },
+    {
+        name: "Redux Toolkit",
+        icon: redux,
+    },
+    {
+        name: "Node JS",
+        icon: nodejs,
+    },
+    {
+        name: "MongoDB",
+        icon: mongodb,
+    },
+    {
+        name: "git",
+        icon: git,
+    },
+];
+
+const experiences = [
+    {
+        title: "Front End Developer",
+        company_name: "Kopegtel Mediatron",
+        icon: mediatron,
+        iconBg: "#383E56",
+        date: "Sept 2021 - Aug 2023",
+        points: [
+            "Mengembangkan dan memelihara aplikasi web menggunakan React.js dan teknologi terkait lainnya.",
+            "Berkolaborasi dengan cross-functional teams termasuk desainer, product managers, dan developers lainnya.",
+            "Menerapkan desain responsif dan memastikan kompatibilitas lintas browser.",
+            "Berpartisipasi dalam peninjauan kode dan memberikan umpan balik yang membangun kepada developers lain.",
+        ],
+    },
+    {
+        title: "Internship Front End Developer",
+        company_name: "PT. Telekomunikasi Indonesia Tbk Direktorat Digital Business",
+        icon: digistar,
+        iconBg: "#E6DEDD",
+        date: "Sept 2020 - Aug 2021",
+        points: [
+            "membuat tampilan program web dari desain yang diberikan dan menghubungkannya dengan software backend sehingga itu bisa digunakan oleh pengguna."
+        ],
+    },
+];
+
+const testimonials = [
+    {
+        testimonial:
+            "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+        name: "Sara Lee",
+        designation: "CFO",
+        company: "Acme Co",
+        image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+        testimonial:
+            "I've never met a web developer who truly cares about their clients' success like Rick does.",
+        name: "Chris Brown",
+        designation: "COO",
+        company: "DEF Corp",
+        image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+        testimonial:
+            "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+        name: "Lisa Wang",
+        designation: "CTO",
+        company: "456 Enterprises",
+        image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+];
+
+const projects = [
+    {
+        name: "Car Rent",
+        description:
+            "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+        tags: [
+            {
+                name: "react",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "mongodb",
+                color: "green-text-gradient",
+            },
+            {
+                name: "tailwind",
+                color: "pink-text-gradient",
+            },
+        ],
+        image: carrent,
+        source_code_link: "https://github.com/",
+    },
+    {
+        name: "Job IT",
+        description:
+            "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+        tags: [
+            {
+                name: "react",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "restapi",
+                color: "green-text-gradient",
+            },
+            {
+                name: "scss",
+                color: "pink-text-gradient",
+            },
+        ],
+        image: jobit,
+        source_code_link: "https://github.com/",
+    },
+    {
+        name: "Trip Guide",
+        description:
+            "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+        tags: [
+            {
+                name: "nextjs",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "supabase",
+                color: "green-text-gradient",
+            },
+            {
+                name: "css",
+                color: "pink-text-gradient",
+            },
+        ],
+        image: tripguide,
+        source_code_link: "https://github.com/",
+    },
+];
+
+export { services, technologies, experiences, testimonials, projects };
